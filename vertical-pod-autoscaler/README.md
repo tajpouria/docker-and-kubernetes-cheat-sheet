c329a8edd623b51cbaddc1bd4d73431330477574
tajpouria <tajpouria.dev@gmail.com>
2022-01-03 10:30:45 +0400

    feat: add vpa basic

---
+++ vertical-pod-autoscaler/README.md
@@ -0,0 +1,1 @@
+https://www.youtube.com/watch\?v\=jcHQ5SKKTLM\&list\=WL\&index\=2\&t\=827s
---
+++ vertical-pod-autoscaler/label-ns.sh
@@ -0,0 +1,1 @@
+kubectl label ns default goldilocks.fairwinds.com/enabled=true goldilocks.fairwinds.com/vpa-update-mode=off
